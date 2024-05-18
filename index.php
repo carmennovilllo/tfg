b6c6d7758b4a423a5d958257fe671732f1685bd6
carmennovilllo <81377181+carmennovilllo@users.noreply.github.com>
2024-05-18 10:42:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php   
-    $pagina = $ GET['page'];
+    $pagina = $_GET['page'];
     include($pagina);
     //mostramos en pantalla un archivo referenciado desde la variable page que se almacenará en la variable pagina y a la función
     //include le pasaremos la variable pagina para que nos la cargue y muestre por pantalla
